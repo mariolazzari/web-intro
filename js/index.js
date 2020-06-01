6bc79396224c2dca1d63fa9c5e72285f66ad69c7
Mario Lazzari <mario.lazzari@gmail.com>
2020-06-01 18:38:52 +0200

    Started javascript chapter2

---
+++ js/index.js
@@ -0,0 +1,13 @@
+// const
+const monthly = 500;
+const yearlyTotal = monthly * 12;
+
+console.log("Total:", yearlyTotal);
+
+// strings
+const myName = "Mario Lazzari";
+console.log(myName);
+// template strings
+const name = "Mario";
+const surname = "Lazzari";
+console.log(`${name} ${surname}`);
